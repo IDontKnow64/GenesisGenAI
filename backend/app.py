23ed5a9030e08af58b379b3620fb21c0eea1cf86
KurbyDoo <moomooman.kurbydoo@gmail.com>
2025-03-22 12:12:21 -0400

    google auth

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,21 @@
+import os
 from flask import Flask
 from flask_cors import CORS
+from flask_session import Session
 from routes.auth import auth_blueprint
 from routes.emails import email_blueprint
 
-app = Flask(__name__)
-CORS(app)
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'  # Use Redis in production
+app.secret_key = os.getenv("GOOGLE_API_KEY")
+Session(app)
+CORS(app)
 
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(email_blueprint, url_prefix='/emails')
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 @app.route('/')
 def home():
