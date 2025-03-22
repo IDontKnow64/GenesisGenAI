29cf8fcaf590a2b86ffb963dc895a26d2d471458
Sarveshwar Senthil Kumar <sarveshwar313@gmail.com>
2025-03-22 10:07:23 -0400

    created tasks to do, and moved main.py contents

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+import os
+import cohere
+from dotenv import load_dotenv
+
+"""
+load_dotenv()
+api_key = os.getenv("CO_API_KEY")
+    
+co = cohere.ClientV2(api_key)
+    
+response = co.chat(
+        model="command-r-plus-08-2024",
+        messages=[{"role": "user", "content": "hello world!"}],
+)
+    
+message_response = response.message.content[0].text
+print(message_response)"""
 
 app = Flask(__name__)
 CORS(app)
