cfcbebb11ae72c1cb353f3bfbd8f8c4c7fba7f23
KurbyDoo <moomooman.kurbydoo@gmail.com>
2025-03-23 08:49:42 -0400

    safety

--- frontend/src/components/CircleChart.tsx
+++ frontend/src/components/CircleChart.tsx
@@ -20,40 +20,20 @@ import {
 } from "@/components/ui/chart"
 import { Button } from "./ui/button"
 const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+  { level: "safety", visitors: 100, fill: "green" },
 ]
 
 const chartConfig = {
   visitors: {
-    label: "Visitors",
+    label: "Score",
   },
-  chrome: {
-    label: "Chrome",
+  level: {
+    label: "safety",
     color: "hsl(var(--chart-1))",
-  },
-  safari: {
-    label: "Safari",
-    color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
   },
 } satisfies ChartConfig
 
-const CircleChart = ({msg_id, description}) => {
+const CircleChart = ({msg_id, description, type, reasons, score}) => {
   const totalVisitors = React.useMemo(() => {
     return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
   }, [])
@@ -96,14 +76,14 @@ const CircleChart = ({msg_id, description}) => {
                           y={viewBox.cy}
                           className="fill-foreground text-3xl font-bold"
                         >
-                          {totalVisitors.toLocaleString()}
+                          {score.toLocaleString()}
                         </tspan>
                         <tspan
                           x={viewBox.cx}
                           y={(viewBox.cy || 0) + 24}
                           className="fill-muted-foreground"
                         >
-                          Visitors
+                          Safety Score
                         </tspan>
                       </text>
                     )
